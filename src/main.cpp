313ff9577b67bbb1f3a69ea2ea8baed2634b54c4
Sanjo Fumihiro <sanjofumihiro@gmail.com>
2025-05-22 07:47:36 +0900

    CoRE終了後最終保存

--- src/main.cpp
+++ src/main.cpp
@@ -1,35 +1,53 @@
 #include "rclcpp/rclcpp.hpp"
-#include "kk_driver_msg/msg/key_ctrl.hpp"
-#include "kk_driver_msg/msg/pwm_cmd.hpp"
-#include "sensor_msgs/msg/joy.hpp"
+#include "kk_driver_msg/msg/motor_cmd.hpp"
 
-class kk_template_node : public rclcpp::Node{
+
+class motor_works_node : public rclcpp::Node{
 public:
-    rclcpp::Subscription<sensor_msgs::msg::Joy>::SharedPtr sub_joy;
-    rclcpp::Subscription<kk_driver_msg::msg::KeyCtrl>::SharedPtr sub_key;
+    rclcpp::Publisher<kk_driver_msg::msg::MotorCmd>::SharedPtr motor_cmd_pub;
+    kk_driver_msg::msg::MotorCmd motor1_cmd_msg;
+    kk_driver_msg::msg::MotorCmd motor2_cmd_msg;
+    rclcpp::TimerBase::SharedPtr timer_;
 
-    kk_template_node() : Node("kk_template_node_node"){
+    float duty[4] = {1.f, 1.f, 1.f, 1.f};
 
-        // Joyコンのサブスクライバー
-        sub_joy = this->create_subscription<sensor_msgs::msg::Joy>(
-            "/joy", rclcpp::QoS(10),
-            std::bind(&kk_template_node::joy_callback, this, std::placeholders::_1)
-        );
-        // キーボードのサブスクライバー (CoRE2用)
-        sub_key = this->create_subscription<kk_driver_msg::msg::KeyCtrl>(
-            "/keys" , rclcpp::QoS(10),
-            std::bind(&kk_template_node::key_callback , this,
-            std::placeholders::_1)
-        );
+    void update(){
+
+        motor1_cmd_msg.child_id = 0;
+        motor1_cmd_msg.port.resize(2);
+        motor1_cmd_msg.ctrl.resize(2);
+        motor1_cmd_msg.target.resize(2);
+
+        motor2_cmd_msg.child_id = 1;
+        motor2_cmd_msg.port.resize(2);
+        motor2_cmd_msg.ctrl.resize(2);
+        motor2_cmd_msg.target.resize(2);
         
+        motor1_cmd_msg.port[0] = 0;
+        motor1_cmd_msg.ctrl[0] = 1;
+        motor1_cmd_msg.target[0] = static_cast<int32_t>(duty[0] * 0xFFFFF);
+
+        motor1_cmd_msg.port[1] = 1;
+        motor1_cmd_msg.ctrl[1] = 1;
+        motor1_cmd_msg.target[1] = static_cast<int32_t>(duty[1] * 0xFFFFF);
+
+        motor2_cmd_msg.port[0] = 0;
+        motor2_cmd_msg.ctrl[0] = 1;
+        motor2_cmd_msg.target[0] = static_cast<int32_t>(duty[2] * 0xFFFFF);
+
+        motor2_cmd_msg.port[1] = 1;
+        motor2_cmd_msg.ctrl[1] = 1;
+        motor2_cmd_msg.target[1] = static_cast<int32_t>(duty[3] * 0xFFFFF);
+        
+        motor_cmd_pub->publish(motor1_cmd_msg);
+        motor_cmd_pub->publish(motor2_cmd_msg);
     }
 
-    // Joyコンの処理
-    void joy_callback(const sensor_msgs::msg::Joy::SharedPtr msg){
-    }
-
-    // キー入力の処理
-    void key_callback(const kk_driver_msg::msg::KeyCtrl::SharedPtr msg){
+    motor_works_node() : Node("motor_works_node_node"){
+        motor_cmd_pub = this->create_publisher<kk_driver_msg::msg::MotorCmd>("mtr/cmd", rclcpp::QoS(10));
+        timer_ = this->create_wall_timer(
+            std::chrono::milliseconds(50), // 1秒ごとに実行
+            std::bind(&motor_works_node::update, this));    
     }
 
 };
@@ -40,7 +58,7 @@ int main(int argc, char *argv[]) {
     // ROS 2クライアントライブラリの初期化
     rclcpp::init(argc, argv);
     // ノードの作成と実行
-    auto node = std::make_shared<kk_template_node>();
+    auto node = std::make_shared<motor_works_node>();
     // スピン処理
     rclcpp::spin(node);
     // ROS 2クライアントライブラリの終了処理
